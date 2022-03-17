4814e13e3dd4251399fde31f02aa812acdd10d49
Admin <Admin@DESKTOP-4D1JTKI>
2022-03-17 09:55:34 +0530

    Latest commit

--- Test Suites/Sample1TestSuite.ts
+++ Test Suites/Sample1TestSuite.ts
@@ -5,7 +5,7 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>1</numberOfRerun>
+   <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
@@ -20,7 +20,7 @@
    <testCaseLink>
       <guid>86b081c4-d1a5-420c-99f1-b43ef6393fa7</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Sample2Manual</testCaseId>
    </testCaseLink>
    <testCaseLink>
